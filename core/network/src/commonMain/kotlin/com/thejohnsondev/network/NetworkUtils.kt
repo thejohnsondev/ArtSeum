1dc3cd2c9bb84c13757463a13ca196a05644a022
Andrew <thejohnsondev@gmail.com>
2026-01-11 15:46:51 +0200

    feat: add ArtApiUtils

--- core/network/src/commonMain/kotlin/com/thejohnsondev/network/NetworkUtils.kt
+++ core/network/src/commonMain/kotlin/com/thejohnsondev/network/NetworkUtils.kt
@@ -1,18 +1,38 @@
 package com.thejohnsondev.network
 
-import io.ktor.http.ContentType
-import io.ktor.http.HttpMessageBuilder
-import io.ktor.http.URLBuilder
-import io.ktor.http.URLProtocol
-import io.ktor.http.contentType
+import com.thejohnsondev.common.HttpError
+import com.thejohnsondev.common.NoInternetConnectionError
+import com.thejohnsondev.common.UnknownError
+import com.thejohnsondev.network.api.ArtApiErrorResponse
+import io.ktor.client.call.body
+import io.ktor.client.statement.HttpResponse
 
-fun URLBuilder.defaultUrlConfig() {
-    protocol = URLProtocol.HTTPS
-    host = BASE_URL
-}
+suspend inline fun <reified T> callWithMapping(call: (() -> HttpResponse)): Result<T> {
+    return try {
+        val response = call.invoke()
+        return when (response.status.value) {
+            in 200..300 -> {
+                Result.success(response.body<T>())
+            }
 
-fun HttpMessageBuilder.defaultRequestConfig(
-    contentType: ContentType = ContentType.Application.Json
-) {
-    contentType(contentType)
+            in 400..600 -> {
+                Result.failure(
+                    HttpError(
+                        response.status.value,
+                        response.body<ArtApiErrorResponse>().error
+                    )
+                )
+            }
+
+            else -> {
+                Result.failure(UnknownError())
+            }
+        }
+    } catch (e: Exception) {
+        if (e is NoInternetConnectionError) {
+            Result.failure(e)
+        } else {
+            Result.failure(UnknownError())
+        }
+    }
 }
