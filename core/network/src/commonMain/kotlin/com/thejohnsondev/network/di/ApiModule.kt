51619e7fd7f5a52b3475c642b6446ad9a7ac88a1
Andrew <thejohnsondev@gmail.com>
2026-01-11 16:13:02 +0200

    feat: add ArtApi to di

--- core/network/src/commonMain/kotlin/com/thejohnsondev/network/di/ApiModule.kt
+++ core/network/src/commonMain/kotlin/com/thejohnsondev/network/di/ApiModule.kt
@@ -1,7 +1,11 @@
 package com.thejohnsondev.network.di
 
+import com.thejohnsondev.network.api.ArtApi
+import com.thejohnsondev.network.api.ArtApiImpl
+import org.koin.core.module.dsl.bind
+import org.koin.core.module.dsl.singleOf
 import org.koin.dsl.module
 
 val apiModule = module {
-    // TODO add api implementations here
+    singleOf(::ArtApiImpl) { bind<ArtApi>() }
 }
