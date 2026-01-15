686c6891ccd6a8d2778e0b46c9faaf43f09d993b
Andrew <thejohnsondev@gmail.com>
2026-01-15 17:04:16 +0200

    feat: add view models to di

--- presentation/src/main/java/com/thejohnsondev/presentation/di/PresentationModule.kt
+++ presentation/src/main/java/com/thejohnsondev/presentation/di/PresentationModule.kt
@@ -1,7 +1,9 @@
 package com.thejohnsondev.presentation.di
 
+import com.thejohnsondev.presentation.ArtListViewModel
+import org.koin.core.module.dsl.viewModelOf
 import org.koin.dsl.module
 
 val presentationModule = module {
-
+    viewModelOf(::ArtListViewModel)
 }
