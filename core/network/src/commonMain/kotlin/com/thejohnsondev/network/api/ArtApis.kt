a68c23f5d064d365d9301972bd645013374046aa
Andrew <thejohnsondev@gmail.com>
2026-01-17 13:34:05 +0200

    fix: fix api extra slash

--- core/network/src/commonMain/kotlin/com/thejohnsondev/network/api/ArtApis.kt
+++ core/network/src/commonMain/kotlin/com/thejohnsondev/network/api/ArtApis.kt
@@ -1,6 +1,6 @@
 package com.thejohnsondev.network.api
 
-const val BASE_URL = "api.artic.edu/"
+const val BASE_URL = "api.artic.edu"
 
 const val URL_ARTWORKS = "api/v1/artworks"
 const val URL_ARTWORKS_SEARCH = "api/v1/artworks/search"
