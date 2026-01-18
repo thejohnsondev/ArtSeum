1863d46a80bd7601a29a196704664b825d0e3081
Andrew <thejohnsondev@gmail.com>
2026-01-18 16:53:17 +0200

    feat: add network modules to koin initializer

--- composeApp/src/commonMain/kotlin/com/thejohnsondev/artseum/KoinInitializer.kt
+++ composeApp/src/commonMain/kotlin/com/thejohnsondev/artseum/KoinInitializer.kt
@@ -2,6 +2,8 @@ package com.thejohnsondev.artseum
 
 import com.thejohnsondev.data.di.dataModule
 import com.thejohnsondev.domain.di.domainModule
+import com.thejohnsondev.network.di.apiModule
+import com.thejohnsondev.network.di.networkModule
 import com.thejohnsondev.presentation.di.presentationModule
 
 expect class KoinInitializer {
@@ -9,6 +11,8 @@ expect class KoinInitializer {
 }
 
 val commonModules = listOf(
+    networkModule,
+    apiModule,
     dataModule,
     domainModule,
     presentationModule
